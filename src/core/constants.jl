a1ace6a590497a3a740b1611a01e0db91a755890
Cora Kingdon <ckingdon@berkeley.edu>
2019-05-10 09:38:54 -0400

    Add some DICE namespace specificity

--- src/core/constants.jl
+++ src/core/constants.jl
@@ -4,9 +4,9 @@
 const scenario_names = ["IMAGE", "MERGE Optimistic", "MESSAGE", "MiniCAM Base", "5th Scenario"]
 
 # Default values for user facing functions
+const _default_year = 2020      # default perturbation year for marginal damages and scc
 const _default_discount = 0.03  # 3%
-const _default_horizon = 2300      # Same as H (the variable name used by the IWG)
-const _default_year = 2020     # default perturbation year for marginal damages and scc
+const _default_horizon = 2300   # Same as H (the variable name used by the IWG)
 const _default_discount_rates = [.025, .03, .05]            # used by MCS
 
 #------------------------------------------------------------------------------
@@ -16,10 +16,10 @@ const _default_discount_rates = [.025, .03, .05]            # used by MCS
 const iwg_dice_input_file = joinpath(@__DIR__, "../../data/IWG_inputs/DICE/SCC_input_EMFscenarios.xls")
 const RBdistribution_file = joinpath(@__DIR__, "../../data/IWG_inputs/DICE/2009 11 23 Calibrated R&B distribution.xls")
 
-const dice_ts = 10
-const dice_years = collect(2005:dice_ts:2405)
+const dice_ts = 10                              # length of DICE timestep: 10 years
+const dice_years = 2005:dice_ts:2405   # time dimension of the IWG's DICE model
 
-const _default_dice_perturbation_years = collect(2005:dice_ts:2295)   # used by MCS
+const _default_dice_perturbation_years = collect(2005:dice_ts:2295)   # used by MCS for SCC calculations
 
 # Input parameters from EPA's Matlab code
 const H     = 2300       # Time horizon for calculating SCC [year]
@@ -28,22 +28,36 @@ const gamma = 0.3        # Labor factor productivity, from DICE2010
 const delta = 0.1        # Capital depreciation rate [yr^-1], from DICE2010
 const s     = 0.23       # Approximate optimal savings in DICE2010 
 
-const inflate = 122.58 / 114.52 # World GDP inflator 2005 => 2007
+const dice_inflate = 122.58 / 114.52 # World GDP inflator 2005 => 2007
 
-const dice_scenario_convert = Dict{String, String}(    # convert to names standard across all three models and consistent with the TSDs
-    "IMAGE"             => "IMAGE",
-    "MERGE Optimistic"   => "MERGEoptimistic",
-    "MESSAGE"           => "MESSAGE",
-    "MiniCAM Base"       => "MiniCAMbase",
-    "5th Scenario"       => "5thScenario"
+const dice_scenario_convert = Dict{String, String}(    # convert from names standard across all three models and consistent with the TSDs to the DICE-specific names used in the input files
+    "IMAGE"            => "IMAGE",
+    "MERGE Optimistic" => "MERGEoptimistic",
+    "MESSAGE"          => "MESSAGE",
+    "MiniCAM Base"     => "MiniCAMbase",
+    "5th Scenario"     => "5thScenario"
 )
+
 
 #------------------------------------------------------------------------------
 # 2. FUND specific constants
 #------------------------------------------------------------------------------
 
+const iwg_fund_datadir = joinpath(@__DIR__, "../../data/IWG_inputs/FUND/")    
 
+const fund_inflator = 1.3839  # 1990(?)$ => 2007$
 
+const fund_years = 1950:2300   # number of years to include for the SCC calculation, even though model is run to 3000
+
+const _default_fund_perturbation_years = collect(2010:5:2050)
+
+const fund_scenario_convert = Dict{String, String}(    # convert from names standard across all three models and consistent with the TSDs to the FUND-specific names used in the input files
+    "IMAGE"             => "IMAGE",
+    "MERGE Optimistic"  => "MERGE Optimistic",
+    "MESSAGE"           => "MESSAGE",
+    "MiniCAM Base"      => "MiniCAM",
+    "5th Scenario"      => "Policy Level Average"
+)
 
 
 #------------------------------------------------------------------------------
