b72143cea63a98fc76f06bc96ffc1f53e3842988
Cora Kingdon <ckingdon@berkeley.edu>
2021-01-21 15:00:57 -0500

    add FUND validation tests

--- test/test_FUND.jl
+++ test/test_FUND.jl
@@ -1,4 +1,11 @@
+using DelimitedFiles
 using MimiIWG
+using Test
+
+validation_dir = joinpath(@__DIR__, "../data/validation/FUND/usg 2013 fund deterministic run results/")
+validation_files = readdir(validation_dir)  # separate file for each scenario and gas
+
+_atol = 1e-3
 
 @testset "FUND" begin 
 
@@ -18,5 +25,61 @@ using MimiIWG
         MimiIWG.run_scc_mcs(FUND, trials=2, output_dir = tmp_dir, domestic=true)
         rm(tmp_dir, recursive=true)
     end
+
+    # Test the deterministic (non Monte Carlo) results from a modal run of FUND against values from the EPA
+    
+    # Carbon dioxide
+    @testset "Deterministic SC-CO2 validation" begin
+        
+        for scen in MimiIWG.scenarios
+            file_idx = findfirst(x -> occursin("$(MimiIWG.fund_scenario_convert[scen]) - C", x), validation_files)
+            scen_file = validation_files[file_idx]
+            scen_validation_values = readdlm(joinpath(validation_dir, scen_file), ',')[3, 2:16]
+        
+            idx = 1
+            for year in 2010:10:2050, dr in [0.025, 0.03, 0.05]
+                mimi_scc = MimiIWG.compute_scc(FUND, scen, gas=:CO2, year=year, discount=dr)
+                iwg_scc = scen_validation_values[idx]
+                @test mimi_scc ≈ iwg_scc atol = _atol
+                idx += 1
+            end
+        end
+    end
+
+    # Methane
+    @testset "Deterministic SC-CH4 validation" begin
+    
+        for scen in MimiIWG.scenarios
+            file_idx = findfirst(x -> occursin("$(MimiIWG.fund_scenario_convert[scen]) - CH4", x), validation_files)
+            scen_file = validation_files[file_idx]
+            scen_validation_values = readdlm(joinpath(validation_dir, scen_file), ',')[3, 2:16]
+        
+            idx = 1
+            for year in 2010:10:2050, dr in [0.025, 0.03, 0.05]
+                mimi_scc = MimiIWG.compute_scc(FUND, scen, gas=:CH4, year=year, discount=dr)
+                iwg_scc = scen_validation_values[idx]
+                @test mimi_scc ≈ iwg_scc atol = _atol
+                idx += 1
+            end
+        end
+    end
+
+    # Nitrous oxide
+    @testset "Deterministic SC-N2O validation" begin
+    
+    for scen in MimiIWG.scenarios
+        file_idx = findfirst(x -> occursin("$(MimiIWG.fund_scenario_convert[scen]) - N2O", x), validation_files)
+        scen_file = validation_files[file_idx]
+        scen_validation_values = readdlm(joinpath(validation_dir, scen_file), ',')[3, 2:16]
+    
+        idx = 1
+        for year in 2010:10:2050, dr in [0.025, 0.03, 0.05]
+            mimi_scc = MimiIWG.compute_scc(FUND, scen, gas=:N2O, year=year, discount=dr)
+            iwg_scc = scen_validation_values[idx]
+            @test mimi_scc ≈ iwg_scc atol = _atol
+            idx += 1
+        end
+    end
+end
 
 end
